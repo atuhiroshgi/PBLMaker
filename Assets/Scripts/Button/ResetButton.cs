c1c632969b2280a9add2ab74709bbde38dab65b3
3110-side <saidoatsuhiro@gmail.com>
2024-11-13 21:03:00 +0900

    リセットボタンを長押ししたときにカウントダウンを表示するよう変更

--- Assets/Scripts/Button/ResetButton.cs
+++ Assets/Scripts/Button/ResetButton.cs
@@ -1,18 +1,27 @@
 using UnityEngine;
+using TMPro;
 
 public class ResetButton : Button
 {
     [SerializeField, Header("GridGeneratorを参照")]
     private GridGenerator gridGenerator = null;
+    [SerializeField, Header("カウントダウン用のテキスト")]
+    private TextMeshProUGUI countdownText = null;
 
     private bool isPressing = false;            // ボタンが押されているかどうか
     private float pressTime = 0f;               // ボタンが押されている時間
     private const float requiredHoldTime = 3f;  // 3秒間押下が必要
 
+    private void Awake()
+    {
+        countdownText.enabled = false;
+    }
+
     protected override void OnPointerDown()
     {
         base.OnPointerDown();
         isPressing = true;
+        countdownText.enabled = true;
         pressTime = 0f;     // タイマーをリセット
     }
 
@@ -20,6 +29,7 @@ public class ResetButton : Button
     {
         base.OnPointerUp();
         isPressing = false;
+        countdownText.enabled = false;
         pressTime = 0f;     // タイマーをリセット
     }
 
@@ -29,11 +39,13 @@ public class ResetButton : Button
         if (isPressing)
         {
             pressTime += Time.deltaTime;
+            countdownText.text = ((int)(3 - pressTime) + 1).ToString();
 
             // 押下時間が指定された時間に達したらOnClick処理を実行
             if (pressTime >= requiredHoldTime)
             {
                 ResetGridCell();
+                countdownText.enabled = false;
                 isPressing = false;     // 処理を一度だけ行うためフラグをオフに
             }
         }
