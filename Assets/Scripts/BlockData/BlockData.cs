f59743f88baaa98d59b4b400a58798fe700f9c8d
3110-side <saidoatsuhiro@gmail.com>
2024-11-20 03:44:06 +0900

    敵や装置などのブロックではないオブジェクトを設置する処理を追加

--- Assets/Scripts/BlockData/BlockData.cs
+++ Assets/Scripts/BlockData/BlockData.cs
@@ -3,12 +3,18 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "BlockData", menuName = "Scriptable Objects/BlockData")]
 public class BlockData : ScriptableObject
 {
+    [SerializeField, Header("プレハブを設定")]
+    private GameObject prefab;
     [SerializeField, Header("ブロックのアイコン画像")]
     private Sprite blockSprite;
     [SerializeField, Header("ブロックに対応したblockIndex")]
     private int blockType;
+    [SerializeField, Header("このデータがブロックかどうか")]
+    private bool isBlock = true;
 
     // データにアクセスするためのプロパティ
+    public GameObject Prefab => prefab;
     public Sprite BlockSprite => blockSprite;
     public int BlockType => blockType;
+    public bool IsBlock => isBlock;
 }
